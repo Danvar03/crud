a2675221f641b56a34c4ed578bac4a2e4d018cf2
Danvar03 <daniela.03v@gmail.com>
2021-02-24 17:38:21 -0500

    finish memory crud

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import shortid from "shortid"
 function App() {
   const [task, setTask] = useState("")
   const [tasks, setTasks] = useState([])
-  const [editMode, setEditMode] = useState(false) 
-  const [id, setId] = useState("")  
- 
+  const [editMode, setEditMode] = useState(false)
+  const [id, setId] = useState("")
+  const [error, setError] = useState(null)
+
+  const validForm =() => {
+    let isValid = true 
+    setError(null)
+
+    if (isEmpty(task)) {
+      setError("Debes de ingresar una tarea")    
+      isValid = false
+    }
+    return isValid
+  }
+
 
   const addTask = (e) => {
     e.preventDefault()
-    if (isEmpty(task)) {
-      console.log("Task empty")
+    
+    if (!validForm()) {
       return
     }
+
     const newTask = {
       id: shortid.generate(),
       name: task,
@@ -57,7 +70,7 @@ function App() {
           <h4 className="text-center">Lista de Tareas</h4>
 
           {size(tasks) == 0 ? (
-            <h5 className="text-center"> Aun no hay tareas programadas</h5>
+            <li className="list-group-item"> Aun no hay tareas programadas</li>
           ) : (
             <ul className="list-group">
               {tasks.map((task) => (
@@ -85,13 +98,16 @@ function App() {
             {editMode ? "Modificar tarea" : "Agregar Tarea"}
           </h4>
           <form onSubmit= {editMode ? saveTask : addTask}>
+            {
+              error && <span className ="text-danger ">{error}</span>
+            }
             <input
               type="text"
               className="form-contol mb-2"
               placeholder="Ingrese la tarea..."
               onChange={(text) => setTask(text.target.value)}
               value={task}
-            />
+            />            
             <button className= {editMode ? "btn btn-warning" : "btn btn-dark btn-block" } 
               type="submit">
               {editMode ? "Guardar" : "Agregar"}
