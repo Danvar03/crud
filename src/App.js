a3d3329d509abf415a011710c03c56df99e8d389
Danvar03 <daniela.03v@gmail.com>
2021-02-23 23:11:40 -0500

    add task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React,{useState} from 'react'
 import { isEmpty } from 'lodash'
+import shortid from 'shortid'
 
 function App() {
   const [task, settask] = useState("")
+  const [tasks, setTasks] = useState([])
 
     const addTask = (e) => {
       e.preventDefault()
       if(isEmpty(task)){
         console.log("Task empty")
         return
-      }
-      console.log("Ok")
+        }
+        const newTask = {
+          id: shortid.generate(),
+          name:task
+        }
+        setTasks([...tasks, newTask])
+      
       settask("")
-
     }
 
   return (
@@ -23,13 +29,16 @@ function App() {
       <div className="col-8">  
       <h4 className="text-center">Lista de Tareas</h4>
       <ul className="list-group">
-      <li className="list-group-item">
-        <span className="lead">Nombre de la tarea</span>
-        <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>    
-        <button className="btn btn-warning btn-sm float-right">Editar</button>
-       
-      </li>
-      </ul>      
+      {
+      tasks.map((task) =>  (
+        <li className="list-group-item" key={task.id}>
+         <span className="lead">{task.name}</span>
+         <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>    
+         <button className="btn btn-warning btn-sm float-right">Editar</button>
+         </li>
+      )) 
+      }
+      </ul> 
    </div>   
       <div className="col-4">
         <h4 className="text-center"> Formulario </h4> 
